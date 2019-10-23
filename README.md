70db3dc0f00af4dc8c857414ebaeb461f9d03360
vbv13 <pjastrzebscy@gmail.com>
2019-10-23 23:30:28 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,10 +1,21 @@
 #Jak uruchomić?
+
+niezbędne środowskio uruchomieniowe node.js
+
+w folderze projektu:
+
 1.npm install
+
 2.npm start
 
 #Co się dzieje?
-pobieram informacje o członkach rodziny po polsku i angielsku
-zostają one zapisane do pliku csv
-w index.js
 
-następnie
+https://pl.wiktionary.org/wiki/Indeks:Angielski_-_Rodzina
+
+pobieram informacje o członkach rodziny po polsku i ich odpowiednikach po angielsku
+zostają one zapisane do pliku csv<br />
+(w index.js)
+
+chodząc po odnośniku każdej z podstron dotyczącej członka rodziny pobierane są 
+opisy jego dotyczące oraz znajdujące się zdjęcia<br />
+(index2.js)
--- index2.js
+++ index2.js
@@ -7,12 +7,10 @@ const url = "https://pl.wiktionary.org/wiki/Indeks:Angielski_-_Rodzina";
 
 async function getFamilyData() {
     const html = await request(url)
-    //console.log(html)
 
     const familyMembers = cheerio('tr > td > a', html)
         .map(async (index, element) => {
-        //console.log(element.children[0].data)
-        const member = element.children[0].data;
+        const member = element.children[0].data
         return {member}
     })
     .get()
@@ -31,12 +29,28 @@ async function getDescription(membersWithHeaders) {
             } catch(error) {
                 console.error(error);
             }
+
+        })
+    )
+}
+
+async function getPhotos(membersWithHeaders) {
+    return await Promise.all(
+        membersWithHeaders.map(async member => {
+            try {
+                const htmlResult = await request.get(member.url);
+                const $ = await cheerio.load(htmlResult);
+                member.photos = $('img').toString('base64');
+                return {member};
+            } catch(error) {
+                console.error(error);
+            }
+
         })
     )
 }
 
 async function createCsvFile(data) {
-    console.log(typeof(data))
     let csv = new ObjectsToCsv(data);
   
     // Save to file:
@@ -46,8 +60,16 @@ async function createCsvFile(data) {
 async function scrapeWiki() {
     const membersWithHeaders = await getFamilyData()
     const membersFullData = await getDescription(membersWithHeaders)
-    console.log(typeof(membersFullData))
-    await createCsvFile(membersFullData)
+
+    const stringData = membersFullData.join(',')
+    const stringDataWithoutLastComma = stringData.substring(0, stringChanged.length - 1)
+    const parseMembersFullData = JSON.parse("[" + stringDataWithoutLastComma + "]")
+    //const parseMembersFullData = Array.from(stringDataWithoutLastComma)
+
+    const membersPhotos = await getPhotos(membersWithHeaders)
+
+    await createCsvFile(parseMembersFullData)
+    await createCsvFile(membersPhotos)
 }
 
 scrapeWiki()
